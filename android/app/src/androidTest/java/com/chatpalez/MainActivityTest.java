b44502c16955ad18fdedc10972adc16b2b5e311f
Justice Clement <justicechinemelum@gmail.com>
2026-09-15 13:00:11 +0100

    test: add Android activity launch smoke test

---
+++ android/app/src/androidTest/java/com/chatpalez/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.chatpalez;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void appContainerLaunchesWithoutCrashing() {
+        onView(isRoot()).check(matches(isDisplayed()));
+    }
+}
